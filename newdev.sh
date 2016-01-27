7a315bdceb91e400e2a74585aaa87c49b9f0225e
Nick Gaswirth <nick@roundhouse-designs.com>
2016-01-27 13:38:16 -0500

    Added WPMUDEV Smart Crawl ('wpmu-dev-seo'); Added WPMUDEV activations (untestedcd scripts/newdev/) and global plugin updates (untestedcd scripts/newdev/)

--- newdev.sh
+++ newdev.sh
@@ -30,6 +30,7 @@ define( 'FORCE_SSL_ADMIN', true );
 \$_SERVER['HTTPS']='on';
 PHP
 wp core install --url="http://dev.roundhouse-designs.com/${DEVDIR}" --title="$TITLE" --admin_user="nick" --admin_password="H961CxwzdYymwIelIRQm" --admin_email="nick@roundhouse-designs.com"
+wp rewrite structure '/%postname%/'
 
 # Finish user creation
 wp user create ryan ryan@roundhouse-designs.com --role="administrator" --first_name="Ryan" --last_name="Foy" --send-email
@@ -68,6 +69,7 @@ cd "$DEVPATH"
 cp -rv /home/gaswirth/resources/plugins/wpmudev/wpmudev-updates wp-content/plugins/
 cp -rv /home/gaswirth/resources/plugins/wpmudev/google-analytics-async wp-content/plugins/
 cp -rv /home/gaswirth/resources/plugins/wpmudev/wp-smush-pro wp-content/plugins/
+cp -rv /home/gaswirth/resources/plugins/wpmudev/wpmu-dev-seo wp-content/plugins
 cp -rv /home/gaswirth/resources/plugins/soliloquy wp-content/plugins
 cp -rv /home/gaswirth/resources/plugins/ninja-forms-mailchimp wp-content/plugins
 
@@ -84,7 +86,8 @@ wp plugin install ninja-forms ajax-thumbnail-rebuild intuitive-custom-post-order
 # Install plugins but don't activate
 wp plugin install akismet w3-total-cache wp-social-likes gotmls
 
-# Activate private plugins
+# Update and activate  private plugins
+wp plugin activate wpmudev-updates wpmu-dev-seo wp-smush-pro google-analytics-async 
 wp plugin update --all --quiet
 
 # Set final permissions
