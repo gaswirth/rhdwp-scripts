322e20f8e13a5dd9f7644979c8c406fdaaae9f39
Nick Gaswirth <nick@roundhouse-designs.com>
2016-05-20 22:33:27 -0400

    variables

--- s3-backup/s3-backup.sh
+++ s3-backup/s3-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-EXCLUDES=/home/gaswirth/scripts/s3-backup/s3.exclude
+DIR=/home/gaswirth/scripts/s3-backup
+EXCLUDES="$DIR"/s3.exclude
 LINODE=$1
 
 echo 'S3 Backup Started'
@@ -7,7 +8,7 @@ date +'%a %b %e %H:%M:%S %Z %Y'
 s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /home s3://linode-$LINODE
 s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /etc s3://linode-$LINODE
 s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /var s3://linode-$LINODE
-dpkg --get-selections > /home/gaswirth/s3-backup-dpkg.list
-s3cmd sync --preserve /home/gaswirth/s3-backup-dpkg.list s3://linode-$LINODE
+dpkg --get-selections > "$DIR"/dpkg.list
+s3cmd sync --preserve "$DIR"/dpkg.list s3://linode-$LINODE
 date +'%a %b %e %H:%M:%S %Z %Y'
 echo 'Finished'
