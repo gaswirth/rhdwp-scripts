a9ad280df06918ab45d24eb13b072705ebb173b8
Nick Gaswirth <nick@roundhouse-designs.com>
2016-05-20 22:27:54 -0400

    Universalized s3-backup script

--- s3-backup/s3-backup.sh
+++ s3-backup/s3-backup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+EXCLUDES=/home/gaswirth/scripts/s3-backup/s3.exclude
+LINODE=$1
+
 echo 'S3 Backup Started'
 date +'%a %b %e %H:%M:%S %Z %Y'
-s3cmd sync --delete-removed --recursive --preserve --exclude-from /home/gaswirth/scripts/s3-backup/s3.exclude /home s3://linode-joanna
-s3cmd sync --delete-removed --recursive --preserve --exclude-from /home/gaswirth/scripts/s3-backup/s3.exclude /etc s3://linode-joanna
-s3cmd sync --delete-removed --recursive --preserve --exclude-from /home/gaswirth/scripts/s3-backup/s3.exclude /var s3://linode-joanna
-dpkg --get-selections > /home/gaswirth/scripts/s3-backup/dpkg.list
-s3cmd sync --preserve /home/gaswirth/scripts/s3-backup/dpkg.list s3://linode-joanna
+s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /home s3://linode-$LINODE
+s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /etc s3://linode-$LINODE
+s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /var s3://linode-$LINODE
+dpkg --get-selections > /home/gaswirth/s3-backup-dpkg.list
+s3cmd sync --preserve /home/gaswirth/s3-backup-dpkg.list s3://linode-$LINODE
 date +'%a %b %e %H:%M:%S %Z %Y'
 echo 'Finished'
