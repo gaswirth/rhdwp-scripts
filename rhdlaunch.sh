f786d53344fd827b7341a49bf22cc591bd96f7c4
Nick Gaswirth <nick@roundhouse-designs.com>
2016-07-13 15:56:32 -0400

    readable variables  and  from  and

--- rhdlaunch.sh
+++ rhdlaunch.sh
@@ -9,17 +9,19 @@ echo "---------------------"
 
 DEVROOT="/var/www/public_html/dev.roundhouse-designs.com/public"
 
+DEVDIR=$1
+DOMAIN=$2
 DEVPATH=$DEVROOT/$1
-DOMAINPATH=/var/www/public_html/$2
+DOMAINPATH=/var/www/public_html/$DOMAIN
 APACHEDIR=/etc/apache2/sites-available
-APACHEFILE=$2.conf
+APACHEFILE=$DOMAIN.conf
 
-DBNAME=$(cat $DEVPATH/wp-config.php | grep "'DB_NAME', '" | awk '{print $2}' | sed -r "s/[\'|\)|;]//g")
-DBUSER=$(cat $DEVPATH/wp-config.php | grep "'DB_USER', '" | awk '{print $2}' | sed -r "s/[\'|\)|;]//g")
-DBPASS=$(cat $DEVPATH/wp-config.php | grep "'DB_PASSWORD', '" | awk '{print $2}' | sed -r "s/[\'|\)|;]//g")
+DBNAME=$(cat $DEVPATH/wp-config.php | grep "'DB_NAME', '" | awk '{print $DOMAIN}' | sed -r "s/[\'|\)|;]//g")
+DBUSER=$(cat $DEVPATH/wp-config.php | grep "'DB_USER', '" | awk '{print $DOMAIN}' | sed -r "s/[\'|\)|;]//g")
+DBPASS=$(cat $DEVPATH/wp-config.php | grep "'DB_PASSWORD', '" | awk '{print $DOMAIN}' | sed -r "s/[\'|\)|;]//g")
 DBPASS_FILTERED=$(echo $DBPASS | sed -e 's/[^a-zA-Z0-9,._+@%/-]/\\&/g; 1{$s/^$/""/}; 1!s/^/"/; $!s/$/"/')
 
-echo "Launching $1 to $2 on $LINODE..."
+echo "Launching $1 to $DOMAIN on $LINODE..."
 
 read -p "Live Linode name: " LINODE
 read -s -p "Database admin password: " DBROOTPASS
@@ -65,8 +67,8 @@ cd $DEVPATH
 wp option update admin_email "$ADMINEMAIL"
 wp core update
 wp plugin update --all
-wp search-replace "https://dev.roundhouse-designs.com/$1" "http://$2"
-wp search-replace "http://dev.roundhouse-designs.com/$1" "http://$2"
+wp search-replace "https://dev.roundhouse-designs.com/$1" "http://$DOMAIN"
+wp search-replace "http://dev.roundhouse-designs.com/$1" "http://$DOMAIN"
 
 # Copy site files from dev to live
 scp -r $DEVPATH gaswirth@$LINODE:/tmp
@@ -82,7 +84,7 @@ sudo rm -rf /tmp/$1
 # Set up Apache to serve new site
 cd $APACHEDIR
 sudo cp _template.conf $APACHEFILE
-sudo sed -i "s/domain\.com/$2/g" $APACHEFILE
+sudo sed -i "s/domain\.com/$DOMAIN/g" $APACHEFILE
 sudo a2ensite $APACHEFILE
 sudo service apache2 reload
 
