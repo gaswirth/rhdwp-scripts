9ba8ad7b5a55b680d11157ce88245307961384e6
Nick Gaswirth <nick@roundhouse-designs.com>
2016-12-13 14:42:00 -0500

    added perms command before and after update

--- rhdupdate.sh
+++ rhdupdate.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 #
-#
 # DO NOT CALL THIS SCRIPT DIRECTLY. Move to ~/scripts and edit directories to suit server
-#
 #
 
 export PATH="/home/gaswirth/npm/bin:/home/gaswirth/.npm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 
-for d in /home/joanna/public_html/*; do
-	if [ -d $d ]
-	then
-		path="$d/public"
-		echo "SITE: $d"
-		sudo -u gaswirth -i -- wp plugin update --all --path=$path --quiet
-		sudo -u gaswirth -i -- wp theme update --all --path=$path --quiet
-		sudo -u gaswirth -i -- wp core update --path=$path --quiet
-		chown -R www-data:www-data $path/wp-content
-		chmod -R 775 $path/wp-content
-	fi
+for D in /var/www/public_html/*; do
+        if [ -d $D ]
+        then
+                DIR="${D}/public"
+                CONT="${DIR}/wp-content"
+                echo "SITE: ${D}"
+                chown -R www-data:www-data ${CONT} && chmod -R 775 ${CONT}
+                sudo -u gaswirth -i -- wp plugin update --all --path=${DIR} --quiet
+                sudo -u gaswirth -i -- wp theme update --all --path=${DIR} --quiet
+                sudo -u gaswirth -i -- wp core update --path=${DIR} --quiet
+                chown -R www-data:www-data ${CONT} && chmod -R 775 ${CONT}
+        fi
 done
