edfe88900425288a050e947bbd04c49d56f3de5a
Nick Gaswirth <admin@roundhouse-designs.com>
2018-06-07 01:09:29 -0400

    basic restic backup added

---
+++ restic-backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export B2_ACCOUNT_ID="4bc42f267688"
+export B2_ACCOUNT_KEY="00244196b19d8095255129a248f5e2eff1fd383a52"
+export RESTIC_REPOSITORY="b2:linode-rhdev"
+export RESTIC_PASSWORD_FILE=".restic"
+
+restic backup /
