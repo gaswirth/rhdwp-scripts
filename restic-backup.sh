0b4bd34ec2773a48941ed8026dbd7fb87cc4af94
Nick Gaswirth <admin@roundhouse-designs.com>
2018-06-07 01:22:37 -0400

    restic backup script written

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+hostname=`hostname`
 
 export B2_ACCOUNT_ID="4bc42f267688"
 export B2_ACCOUNT_KEY="00244196b19d8095255129a248f5e2eff1fd383a52"
-export RESTIC_REPOSITORY="b2:linode-rhdev"
+export RESTIC_REPOSITORY="b2:linode-${hostname}"
 export RESTIC_PASSWORD_FILE=".restic"
 
 restic backup /
