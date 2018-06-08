1c5a64b4bd4e7929024f852046050cfd758f545d
Nick Gaswirth <nick@roundhouse-designs.com>
2018-06-07 23:15:33 -0400

    tweaks

--- runme.sh
+++ runme.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "*****************"
-echo " B2 Backup Setup"
-echo "*****************"
+echo "*******************"
+echo "* B2 Backup Setup *"
+echo "*******************"
 
 HN=`hostname`
+PUBKEY=`cat /home/gaswirth/.ssh/id_rsa.pub`
 
 read -n 1 -s -r -p "Please create a 'linode-$HN' B2 Bucket, then press any key to continue..."
 echo ""
@@ -21,13 +22,14 @@ export B2_ACCOUNT_KEY="00244196b19d8095255129a248f5e2eff1fd383a52"
 restic -r "b2:linode-$HN" init
 touch .restic
 
-read -p "Save the following B2 Bucket password in .restic: " BUCKETPASS
+read -s -p "Save the following B2 Bucket password in .restic: " BUCKETPASS
 echo "$BUCKETPASS" > .restic
 chmod 400 .restic
 
 echo "***********"
 echo "** TASKS **"
 echo "***********"
+
 echo "-----------"
 echo "PLEASE EDIT apache2.conf AND CHANGE AllowOverride None in /var/www/ vhost to AllowOverride All"
 echo "-----------"
