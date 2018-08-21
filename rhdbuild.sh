f87f291ec96dc66bd92576fc021263be5d185d8e
Nick Gaswirth <admin@roundhouse-designs.com>
2018-08-21 15:16:49 -0400

    rhdbuild now based on new RHDWP theme framework

--- rhdbuild.sh
+++ rhdbuild.sh
@@ -6,7 +6,7 @@ echo "*****************"
 
 read -p "Site Title: " TITLE
 read -p "Site root (no namespace prefixes): " PROJNAME
-read -p "Theme directory name (namespace prefixes ok): " THEMEDIR
+read -p "Child Theme name: " THEMENAME
 echo "*****************"
 read -p "Database name: " DBNAME
 read -p "Database user: " DBUSER
@@ -14,13 +14,19 @@ read -s -p "Database password: " DBPASS
 echo ""
 echo "*****************"
 read -p "GitHub repository name: " REPONAME
-read -p "Branch: " BRANCH
-read -n 1 -s -r -p "Please create a '$REPONAME' GitHub repo or '$BRANCH' remote branch, then press any key to continue..."
+read -n 1 -s -r -p "Please create a '$REPONAME' GitHub repository, then press any key to continue..."
 echo ""
 
 echo "*******************"
 echo "** Rock and roll **"
 echo "******************"
+
+# Vars
+ROOTPATH=/var/www/public_html/
+SITEROOT="$ROOTPATH$PROJNAME"
+CONTENTDIR="$SITEROOT"/wp-content/
+THEMESDIR="$CONTENTDIR"/themes
+THEMESLUG=${THEMENAME// /-} && THEMESLUG=${THEMESLUG,,}
 
 # MYSQL SETUP
 echo "mySQL setup..."
@@ -31,15 +37,13 @@ FLUSH PRIVILEGES;
 EOF
 
 ## SITE FILES
-ROOTPATH=/var/www/public_html/
-SITEROOT="$ROOTPATH$PROJNAME"
 mkdir "$SITEROOT"
 
 ## WORDPRESS
 # Set up and install with wp-cli
 echo "WordPress setup..."
 cd "$SITEROOT"
-wp core download --skip-content && wp core config --dbname="$DBNAME" --dbprefix="rhd_wp_" --dbuser="$DBUSER" --dbpass="$DBPASS" --extra-php << PHP 
+wp core download && wp core config --dbname="$DBNAME" --dbprefix="rhd_wp_" --dbuser="$DBUSER" --dbpass="$DBPASS" --extra-php << PHP 
 /* Added by Roundhouse Designs */
 define( 'WPLANG', '');
 define( 'WP_DEBUG_LOG', true );
@@ -51,55 +55,58 @@ define( 'WP_AUTO_UPDATE_CORE', true );
 /* End Roundhouse Designs */
 PHP
 
-# Clone RHD Hannah and mirror to new repo
-echo "GitHub repository setup..."
-mkdir wp-content && cd wp-content
-
-git clone -b "$BRANCH" --single-branch git@github.com:gaswirth/rhdwp-hannah.git rhdtemp
-mv rhdtemp/themes .
-mv rhdtemp/mu-plugins .
-git init
-git remote add origin git@github.com:gaswirth/"$REPONAME".git
-rm -rf rhdtemp
-
 # Complete WP install
 echo "Installing WordPress..."
-wp core install --url="http://dev.roundhouse-designs.com/${PROJNAME}" --title="$TITLE" --admin_user="nick" --admin_password="H961CxwzdYymwIelIRQm" --admin_email="nick@roundhouse-designs.com"
+wp core install --url="http://dev.roundhouse-designs.com/${PROJNAME}" --title="$TITLE" --admin_user="nick" --admin_email="nick@roundhouse-designs.com"
 
 # Generate .htaccess and set rewrite structure
-wp rewrite flush --hard
 wp rewrite structure '/%postname%/'
+wp rewrite flush --hard
 
 ## THEME
-# Initialize Yarn and install Grunt + dependencies
-echo "Theme setup..."
-mv themes/rhd-hannah themes/"$THEMEDIR" && cd themes/"$THEMEDIR"
-yarn init
-sed -ri "s/\"name\": \"rhdwp-hannah\"/\"name\": \"$REPONAME\"/" package.json
-sed -ri "s/rhdwp-hannah.git/$REPONAME/" package.json
-yarn add grunt grunt-contrib-stylus grunt-contrib-watch grunt-contrib-jshint
+# Retrieve RHDWP base theme
+echo "Installing RHDWP Base Theme..."
+cd "$THEMESDIR"
+rm -rf twenty*
+
+# Download the RHDWP base
+git clone git@github.com:gaswirth/rhdwp.git rhdwp
+
+# Create the child theme
+wp scaffold child-theme "$THEMESLUG" --parent_theme=rhdwp --author="Roundhouse Designs" --author_uri=https://roundhouse-designs.com --theme_uri=https://github.com/gaswirth/"$REPONAME".git --activate
+cd "$THEMESDIR"/"$THEMESLUG"
+sed -i "s/$THEMESLUG/$THEMENAME/gI" style.css
+cp "$THEMESDIR"/rhdwp/Gruntfile.js .
+cp "$THEMESDIR"/rhdwp/.gitignore .
+mkdir css
+mkdir stylus
+
+# Yarn setup
+yarn config set init-license GPL-2.0
+yarn config set init-main Gruntfile.js
+yarn config set init-version 1.0
+yarn init --yes 
+yarn add grunt grunt-contrib-stylus grunt-contrib-watch grunt-contrib-jshint livereload-js --dev
 yarn install
 
-# While we're still in the theme dir, change SITEBASE placeholders to dev directory name for Stylus vars
-# We'll also change the main site name in style.css and generate some base stylesheets
-echo "Customize labels..."
-sed -i 's/SITEBASE/"$PROJNAME"/g' assets/stylus/global.styl
-sed -ri "s/Theme Name: (.*?)/Theme Name: RHD $TITLE/" style.css
-sed -ri "s/Description: (.*?)/Description: A custom WordPress theme for $TITLE by Roundhouse Designs/" style.css
-grunt stylus:dev
+# Set up GitHub repository
+echo "Initializing GitHub repository..."
+git init
+git add *
+git commit -m "Initial commit"
+git remote add origin git@github.com:gaswirth/"$REPONAME".git
+git push -u origin master
 
-# Activate the theme, create the primary nav menu, and add the Sample Page for display
-wp theme activate "$THEMEDIR"
-wp menu create "Site Navigation"
-wp menu location assign "Site Navigation" primary
-wp menu item add-post 2 --title="Sample"
+# Create the primary nav menu, and add the Sample Page for display
+# wp menu create "Main Navigation"
+# wp menu location assign "Site Navigation" primary
+# wp menu item add-post 2 --title="Sample"
 
 # Make sure to "exit" back to the root dir.
 cd "$SITEROOT"
 
 ## PLUGINS
 echo "Installing plugins..."
-mkdir wp-content/plugins
 
 # Install WPMUDEV + Dashboard
 cp -rv /home/gaswirth/resources/plugins/wpmudev-updates wp-content/plugins/
@@ -109,7 +116,7 @@ cp -rv /home/gaswirth/resource/plugins/wp-defender wp-content/plugins
 cp -rv /home/gaswirth/resources/plugins/soliloquy wp-content/plugins
 
 # Install and activate plugins
-wp plugin install ajax-thumbnail-rebuild enable-media-replace tinymce-advanced force-strong-passwords social-warfare schema --activate
+wp plugin install ajax-thumbnail-rebuild enable-media-replace force-strong-passwords social-warfare schema --activate
 
 # Plugins for install only
 # (None)
@@ -118,12 +125,6 @@ wp plugin install ajax-thumbnail-rebuild enable-media-replace tinymce-advanced f
 echo "Installing private plugins..."
 wp plugin activate wpmudev-updates wp-smush-pro
 wp plugin update --all --quiet
-
-# Add installed plugins to the repo, commit changes, and push to remote
-cd "$SITEROOT"/wp-content
-git add *
-git commit -m "Initial commit"
-git push -u origin master
 
 # Finish user creation
 echo "Create users..."
