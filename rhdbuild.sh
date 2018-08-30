b98c853a8ac5809e30521b50ee36003d9da30d2d
Nick Gaswirth <admin@roundhouse-designs.com>
2018-08-30 01:53:21 -0400

    restored rhdbuild

--- rhdbuild.sh
+++ rhdbuild.sh
@@ -1,306 +1,145 @@
 #!/bin/bash
-# <UDF name="hostname" label="Hostname" />
-# <UDF name="userpassword" label="User password" />
-# <UDF name="sharedenv" label="Shared environment?" example="Y/N" default="N" />
-# <UDF name="db_password" Label="MySQL root Password" />
-# <UDF name="db_name" Label="Create Database" default="" example="Optionally create this database" />
-# <UDF name="db_user" Label="Create MySQL User" default="" example="Optionally create this user" />
-# <UDF name="db_user_password" Label="Create MySQL User Password" default="" example="User's password" />
-# <UDF name="domain" Label="Site Domain" example="Example: domain.com" default="default" />
-# <UDF name="interactive" Label="Interactive dpkg?" example="Y/N (default: N)" default="" />
-# <UDF name="php_ver" Label="PHP Version" example="7.1 (default: 7.1)" default="7.1" />
 
-source <ssinclude StackScriptID="1">
+echo "*****************"
+echo "** Site basics **"
+echo "*****************"
 
-function rhd_initial_setup {
-	# System Updates (non-interactive)
-	if [ "$INTERACTIVE" = "y" ] || [ "$INTERACTIVE" = "Y" ]; then
-		apt update
-		apt upgrade
-		apt dist-upgrade
-	else
-		export DEBIAN_FRONTEND=noninteractive
-		apt update -y
-		apt upgrade -y
-		apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -y 
-	fi
-	
-	# Essential installs
-	apt install -y postfix git ufw mailutils screen software-properties-common python-software-properties wget letsencrypt less man-db
-	
-	# Git setup
-	git config --global user.email "nick@roundhouse-designs.com"
-	git config --global user.name "Nick Gaswirth"
-	git config --global push.default simple
-	
-	# Services
-	echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
-	echo "postfix postfix/mailname string $FQDN" | debconf-set-selections
-	
-	# Firewall
-	ufw default deny incoming
-	ufw default allow outgoing
-	ufw allow ssh
-	ufw allow http
-	ufw allow https
-	ufw enable
-}
+read -p "Site Title: " TITLE
+read -p "Site root (no namespace prefixes): " PROJNAME
+read -p "Child Theme name: " THEMENAME
+echo "*****************"
+read -p "Database name: " DBNAME
+read -p "Database user: " DBUSER
+read -s -p "Database password: " DBPASS
+echo ""
+echo "*****************"
+read -p "GitHub repository name: " REPONAME
+read -n 1 -s -r -p "Please create a '$REPONAME' GitHub repository, then press any key to continue..."
+echo ""
 
+echo "*******************"
+echo "** Rock and roll **"
+echo "******************"
 
-function rhd_users_setup {
-	# Users
-	# Set up gaswirth and foy users
-	apt install -y sudo
-	adduser gaswirth --disabled-password --gecos ""
-	echo -e "$USERPASSWORD\n$USERPASSWORD" | passwd gaswirth
-	usermod -aG sudo gaswirth
-	
-	adduser foy --disabled-password --gecos ""
-	echo "foy:$(cat /dev/urandom | tr -cd [:graph:] | head -c 12)" | chpasswd
-	usermod -aG sudo foy
-	
-	# Install user pubkeys
-	# Generate SSH keys for gaswirth
-	mkdir {/home/gaswirth/.ssh,/home/foy/.ssh}
-	touch {/home/gaswirth/.ssh/authorized_keys,/home/foy/.ssh/authorized_keys}
-	chmod 700 /home/gaswirth/.ssh /home/foy/.ssh
-	chmod 600 /home/gaswirth/.ssh/authorized_keys /home/foy/.ssh/authorized_keys
-	chown -R gaswirth:gaswirth /home/gaswirth/.ssh
-	chown -R foy:foy /home/foy/.ssh
-	sudo -u gaswirth ssh-keygen -f /home/gaswirth/.ssh/id_rsa -N ''
-	
-	#gaswirth
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC79KWiV0WjBJV8c6AgJnonh12jlVr//3e23of+ADCfgNMasIB8UT9H+xM1zeBh8GmzsrrI4FfiGhhvawa6gyFgbJsSReKWJbsoqFYzUYTDulECsO/iTO438lYIdu4Jsobm+1UsNOX6p3u2zXCrfj1ItLUSGT/dFwTizZIXmktrrg8icW3uFcPieVUqC1qVGsznIgYPDUhPIDLeEELzU87fOfy49YuTZ/M10a4Y8zIv9klzno27RZm90OTA4eqZ1VA+7soWo3TREzhY3X/ZQHC2kdhaseAgUdPfm1hxFg7Cc+Czs6+lAwcA1WhPMAdBKteq5ovbAvphOpQIuVVPkQ+1 gaswirth@Frankengassy.local" >> /home/gaswirth/.ssh/authorized_keys
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbTWzq9yXe8A52N/HzINynxX+miY1zLoh2yTKXxzaJ/OdZGcpMFXWTdy8X+RHtOBIzrOD8oNeQGnEZVzKOYnJQBM1UhUztuJkjn2BM5KCh4LhKOhdn6XjrF8WfKN3P/rcjG0E9IREMKDvtQvdFIqaC2FNkKnVDtan5l3ga1493pckImDO1+E+qwfc3n/XsXj9kHXJ4di9CQ7Y8FI/ODvRq2m+/cZea9RZqHSPYjzhoH2/ol9w5ihLYb9L6sEEaGHn/Xr03bS5paAGV6QG4l2+4UuefLfrgUv4oYx1VVHwejrg26ickU9tuOFCvrOq4eQq4VTCkmEzYtFf/TdhCg33/ gaswirth@Gassybook.local" >> /home/gaswirth/.ssh/authorized_keys
-	
-	#foy
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTvprX8q2H13YwUtbtaIiBk6MT8is8cb4IJJitZQ3DrQ4IpmLulepxKFPdAqzUE2wxCfU8xmXzC2uIq/VGp02dbVOzT2bHyi9XAJTGX4s7hsVyACPXeSHW30dpdHYrLPbEBLx9TY7ODg/sZ/wLb7OAACglLHlxn7/KkRfWyS2XpCWjCzpLK2koC2NpvAFqyue18cBZ37Jm/fIArPiiYxV3JCzAfkStAf14iv4UjRZ0UyJqN2XCYML+Lplo2ltvSffRYTuFxrzszJNkfLuKVx5Gagx7V4P/+GYKpBLAmHpIKTQjEoIqxcA7kNfGOBIAWK21z9ZzlRQGnhO6kqXOPhur foy@Ryan-Foys-Macbook-Pro-2.local" > /home/foy/.ssh/authorized_keys
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDnr84P+XxFziw1jz+UrJjxIp+1ORttNkJxUtmnsBIhyMJkub7OZwCfHsZzjp1rIsTx8lP0TPIR7yl77QZ0e/O9v6Jtc/De2UkI0vaoahjSmt6MEfezVuz+V0zh0VLBZ2GKzBEU2sNW8KqkVvgyeIASDLFMd57jJDk0UxPsTaShIbYDvzPrXId44z9J6r3BrmzFVFrt3CmF1LBaxNasyOXN46MURzQ/EPWAO8r99yvHrsNGmOGrC5HFdS5FXP2fgFttj9laERieQ4UAj5jSGyl7+mEtLaPP2tGDpHTsRCjMHM6O9JV62d8kuf9MPGnKuDQM+4Ifo3zlUv/9hCvFT9Y1 foy@Ryan-Foys-iMac.local" >> /home/foy/.ssh/authorized_keys
-	
-	# Kill SSH root login
-	ssh_disable_root
-}
+# Vars
+ROOTPATH=/var/www/public_html/
+SITEROOT="$ROOTPATH$PROJNAME"
+CONTENTDIR="$SITEROOT"/wp-content/
+THEMESDIR="$CONTENTDIR"/themes
+THEMESLUG=${THEMENAME// /-} && THEMESLUG=${THEMESLUG,,}
 
+# MYSQL SETUP
+echo "mySQL setup..."
+mysql << EOF
+CREATE DATABASE $DBNAME;
+GRANT ALL PRIVILEGES ON $DBNAME.* TO "$DBUSER"@'localhost' IDENTIFIED BY '$DBPASS';
+FLUSH PRIVILEGES;
+EOF
 
-function rhd_runme_script {
-	cd /home/gaswirth
-	wget https://raw.githubusercontent.com/gaswirth/rhdwp-scripts/forked-repos/runme.sh
-	chmod +x runme.sh
-}
+## SITE FILES
+mkdir "$SITEROOT"
 
+## WORDPRESS
+# Set up and install with wp-cli
+echo "WordPress setup..."
+cd "$SITEROOT"
+wp core download && wp core config --dbname="$DBNAME" --dbprefix="rhd_wp_" --dbuser="$DBUSER" --dbpass="$DBPASS" --extra-php << PHP 
+/* Added by Roundhouse Designs */
+define( 'WPLANG', '');
+define( 'WP_DEBUG_LOG', true );
+define( 'FORCE_SSL_ADMIN', true );
+define( 'EMPTY_TRASH_DAYS', 30 );
+define( 'WP_MEMORY_LIMIT', '64M' );
+define( 'WP_MAX_MEMORY_LIMIT', '96M' );
+define( 'WP_AUTO_UPDATE_CORE', true );
+/* End Roundhouse Designs */
+PHP
 
-function rhd_cron_setup {
-	# Update
-	git clone https://github.com/gaswirth/rhdwp-wp_helper.git /usr/local/bin/rhd_wp_helper
-	chown -R www-data:www-data /usr/local/bin/rhd_wp_helper
-	chmod 774 /usr/local/bin/rhd_wp_helper/*.sh
-	
-	if [ "$SHAREDENV" = "y" ] || [ "$SHAREDENV" = "Y" ]; then	
-		HELPER="run_wp_helper.sh"
-	else
-		HELPER="wp_helper.sh"
-	fi
-	
-	# Install Restic (backups)
-	cd /tmp
-	wget https://github.com/restic/restic/releases/download/v0.9.0/restic_0.9.0_linux_amd64.bz2
-	bunzip2 restic_0.9.0_linux_amd64.bz2
-	mv restic_0.9.0_linux_amd64 /usr/local/bin/restic
-	chmod 700 /usr/local/bin/restic
-	chown gaswirth:gaswirth /usr/local/bin/restic
-	
-	# mysqldump passwordless for gaswirth and root (for restic backup script)
-	cat > /home/gaswirth/.my.cnf <<- EOF
-		[mysqldump]
-		user=root
-		password="$DB_PASSWORD"
-	EOF
-	chown gaswirth:gaswirth /home/gaswirth/.my.cnf
-	chmod 600 /home/gaswirth/.my.cnf
-	cp /home/gaswirth/.my.cnf /root/.my.cnf
-	chown root:root /root/.my.cnf
-	
-	# Cron jobs
-	# root
-	cat > /var/spool/cron/crontabs/root <<- EOF
-		0 0 1,15 * * letsencrypt renew --agree-tos --m admin@roundhouse-designs.com
-		0 */6 * * * bash /home/gaswirth/scripts/restic-backup.sh >/dev/null 2>&1
-	EOF
-	
-	# www-data
-	cat > /var/spool/cron/crontabs/www-data <<- EOF
-		30 6 * * * /usr/local/bin/rhd_wp_helper/$HELPER
-	EOF
-	
-	rhd_runme_script
-}
+# Complete WP install
+echo "Installing WordPress..."
+wp core install --url="http://dev.roundhouse-designs.com/${PROJNAME}" --title="$TITLE" --admin_user="nick" --admin_email="nick@roundhouse-designs.com"
 
+# Generate .htaccess and set rewrite structure
+wp rewrite structure '/%postname%/'
+wp rewrite flush --hard
 
-function rhd_environment_setup {
-	# Apache2
-	apt install -y apache2
-	a2dissite 000-default.conf
-	rhd_apache_tune 40
-	mkdir /var/www/{public,log}
-	rm -rf /var/www/html
-	usermod -aG www-data gaswirth
-	
-	# Apache2 AllowOverride All on /var/www
-	cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
-	sed '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride All/AllowOverride None/' /etc/apache2/apache2.conf
-	
-	# PHP + MySQL
-	add-apt-repository ppa:ondrej/php -y
-	apt update
-	apt install -y php"${PHP_VER}" libapache2-mod-php"${PHP_VER}" php"${PHP_VER}"-mysql php"${PHP_VER}"-memcached php"${PHP_VER}"-json php"${PHP_VER}"-mcrypt php"${PHP_VER}"-mbstring php"${PHP_VER}"-xml php"${PHP_VER}"-xmlrpc php"${PHP_VER}"-curl php"${PHP_VER}"-gd php"${PHP_VER}"-imagick
-	
-	# MySQL
-	if [ ! -z "$DB_PASSWORD" ]; then
-		mysql_install "$DB_PASSWORD" && mysql_tune 40
-		
-		if [ ! -z "$DB_USER" ] || [ ! -z "$DB_USER_PASSWORD" ]; then
-			mysql_create_database "$DB_PASSWORD" "$DB_NAME"
-			mysql_create_user "$DB_PASSWORD" "$DB_USER" "$DB_USER_PASSWORD"
-			mysql_grant_user "$DB_PASSWORD" "$DB_USER" "$DB_NAME"
-		fi
-	fi
-	
-	# MySQL customizations
-	cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
-	sed -i -e 's/query_cache_limit/#query_cache_limit/' /etc/mysql/mysql.conf.d/mysqld.cnf
-	sed -i -e 's/query_cache_size/#query_cache_size/' /etc/mysql/mysql.conf.d/mysqld.cnf
-	echo "" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-	echo "# RHD" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-	echo "query_cache_size = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-    echo "query_cache_type =0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-    echo "query_cache_limit = 16M" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-    echo "join_buffer_size = 512K" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-    echo "tmp_table_size = 32M" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-    echo "max_heap_table_size = 32M" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-    echo "innodb_log_file_size = 16M" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-	
-	# Node.js 8.x + Yarn
-	curl -sL https://deb.nodesource.com/setup_8.x | bash -
-	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-	echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-	apt update && apt install -y nodejs yarn
-	yarn global add grunt-cli
-	
-	# WP-CLI
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod 774 wp-cli.phar
-	chown www-data:www-data wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
-}
+## THEME
+# Retrieve RHDWP base theme
+echo "Installing RHDWP Base Theme..."
+cd "$THEMESDIR"
+rm -rf twenty*
 
-function rhd_apache_tune {
-	# Tunes Apache's memory to use the percentage of RAM you specify, defaulting to 70%
-	
-	# $1 - the percent of system memory to allocate towards Apache
-	
-	if [ ! -n "$1" ];
-		then PERCENT=70
-		else PERCENT="$1"
-	fi
-	
-	apt -y install apache2-mpm-prefork libapache2-modsecurity
-	a2enmod expires ssl rewrite headers security2
-	PERPROCMEM=15 # the amount of memory in MB each apache process is likely to utilize
-	MEM=$(grep MemTotal /proc/meminfo | awk '{ print int($2/1024) }') # how much memory in MB this system has
-	MAXCLIENTS=$((MEM*PERCENT/100/PERPROCMEM)) # calculate MaxClients
-	MAXCLIENTS=${MAXCLIENTS/.*} # cast to an integer
-	sed -i -e "s/\(^[ \t]*MaxClients[ \t]*\)[0-9]*/\1$MAXCLIENTS/" /etc/apache2/apache2.conf
-}
+# Download the RHDWP base
+git clone git@github.com:gaswirth/rhdwp.git rhdwp
 
+# Create the child theme
+wp scaffold child-theme "$THEMESLUG" --parent_theme=rhdwp --author="Roundhouse Designs" --author_uri=https://roundhouse-designs.com --theme_uri=https://github.com/gaswirth/"$REPONAME".git --activate
+cd "$THEMESDIR"/"$THEMESLUG"
+sed -i "s/$THEMESLUG/$THEMENAME/gI" style.css
+cp "$THEMESDIR"/rhdwp/Gruntfile.js .
+cp "$THEMESDIR"/rhdwp/.gitignore .
+mkdir css
+mkdir stylus
 
-function rhd_vhost_setup {
-	if [ ! -z "$DOMAIN" ]; then
-	
-		#VirtualHost
-		if [ -e "/etc/apache2/sites-available/"$DOMAIN".conf" ]; then
-			echo "/etc/apache2/sites-available/"$DOMAIN".conf already exists"
-		else
-			echo "# domain: $DOMAIN" > /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "# public: /var/www/public/" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	ServerName $HOSTNAME" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	ServerAlias www.$HOSTNAME" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	DirectoryIndex index.html index.php" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	DocumentRoot /var/www/public" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	LogLevel warn" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	ErrorLog /var/www/log/error.log" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "  CustomLog /var/www/log/access.log combined" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "</VirtualHost>" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "<VirtualHost *:443>" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "  ServerName $HOSTNAME" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "  ServerAlias www.$HOSTNAME" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "  DirectoryIndex index.html index.php" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "  DocumentRoot /var/www/public" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	LogLevel warn" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	ErrorLog /var/www/log/error.log" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	CustomLog /var/www/log/acess.log combined" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	# SSLEngine On" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	# SSLCertificateFile /etc/letsencrypt/live/$HOSTNAME/fullchain.pem" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "	# SSLCertificateKeyFile /etc/letsencrypt/live/$HOSTNAME/privkey.pem" >> /etc/apache2/sites-available/"$DOMAIN".conf
-			echo "</VirtualHost>" >> /etc/apache2/sites-available/"$DOMAIN".conf
-		fi
-		a2ensite "$DOMAIN".conf
-	
-	fi
-}
+# Yarn setup
+yarn config set init-license GPL-2.0
+yarn config set init-main Gruntfile.js
+yarn config set init-version 1.0
+yarn init --yes 
+yarn add grunt grunt-contrib-stylus grunt-contrib-watch grunt-contrib-jshint livereload-js --dev
+yarn add fitvids --save
+yarn install
 
+# Set up GitHub repository
+echo "Initializing GitHub repository..."
+git init
+git add *
+git commit -m "Initial commit"
+git remote add origin git@github.com:gaswirth/"$REPONAME".git
+git push -u origin master
 
-function rhd_goodstuff {
-	# apt cleanup
-	apt autoclean
-	apt autoremove
+# Create the primary nav menu, and add the Sample Page for display
+# wp menu create "Main Navigation"
+# wp menu location assign "Site Navigation" primary
+# wp menu item add-post 2 --title="Sample"
 
-	# iTerm Shell Integration (macOS)
-	curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
-	
-	# Ensure proper permissions, just in case I fucked something up...
-	chown gaswirth:gaswirth /home/gaswirth
-	chown foy:foy /home/foy
-	
-	# Restart services
-	systemctl restart apache2 
+# Make sure to "exit" back to the root dir.
+cd "$SITEROOT"
 
-	# Nick's Aliases
-	echo "alias a2restart='sudo systemctl restart apache2.service'" >> /home/gaswirth/.bash_aliases
-	echo "alias a2reload='sudo systemctl reload apache2.service'" >> /home/gaswirth/.bash_aliases
-	echo "alias update='sudo apt-get update && sudo apt-get upgrade --show-upgraded --assume-yes'" >> /home/gaswirth/.bash_aliases
-	echo "alias perms='sudo chown -R www-data:www-data . && sudo chmod -R 664 * && sudo find . -type d -exec sudo chmod 775 {} \;'" >> /home/gaswirth/.bash_aliases
-	echo "alias a2buddy='curl -sL https://raw.githubusercontent.com/richardforth/apache2buddy/master/apache2buddy.pl | sudo perl'" >> /home/gaswirth/.bash_aliases
+## PLUGINS
+echo "Installing plugins..."
 
-	# `rhd` shared/private setup
-	if [ "$SHAREDENV" = "y" ] || [ "$SHAREDENV" = "Y" ]; then
-		echo "alias rhd='cd /var/www/public_html'" >> /home/gaswirth/.bash_aliases
-	else
-		echo "alias rhd='cd /var/www/public'" >> /home/gaswirth/.bash_aliases
-	fi
+# Install WPMUDEV + Dashboard
+cp -rv /home/gaswirth/resources/plugins/wpmudev-updates wp-content/plugins/
+cp -rv /home/gaswirth/resources/plugins/wp-smush-pro wp-content/plugins/
+cp -rv /home/gaswirth/resources/plugins/wp-hummingbird wp-content/plugins
+cp -rv /home/gaswirth/resource/plugins/wp-defender wp-content/plugins
+cp -rv /home/gaswirth/resources/plugins/soliloquy wp-content/plugins
 
-	chown gaswirth:gaswirth .bash_aliases
-}
+# Install and activate plugins
+wp plugin install ajax-thumbnail-rebuild enable-media-replace force-strong-passwords social-warfare schema --activate
 
-##################
-# Start Script
-##################
-IP=$(system_primary_ip)
-IPV6=$(ifconfig eth0 | awk '/inet6 addr: .+\/64 Scope:Global/{print $3}' | cut -d'/' -f 1)
-FQDN="$HOSTNAME.roundhouse-designs.com"
+# Plugins for install only
+# (None)
 
-# System setup
-hostnamectl set-hostname "$HOSTNAME"
-echo "$IP	$FQDN	$HOSTNAME" >> /etc/hosts
-echo "$IPV6	$FQDN	$HOSTNAME" >> /etc/hosts
+# Update and activate private plugins
+echo "Installing private plugins..."
+wp plugin activate wpmudev-updates wp-smush-pro
+wp plugin update --all --quiet
 
-# Roll it out
-rhd_initial_setup
-rhd_users_setup
-rhd_environment_setup
-rhd_cron_setup
-rhd_vhost_setup
-rhd_goodstuff
+# Finish user creation
+echo "Create users..."
+wp user create ryan ryan@roundhouse-designs.com --role="administrator" --first_name="Ryan" --last_name="Foy" --send-email
+wp user update nick --first_name="Nick" --last_name="Gaswirth"
+wp user update nick ryan --user_url="https://roundhouse-designs.com"
+
+# Set final permissions
+echo "Finalizing..."
+cd "$SITEROOT"
+sudo chmod -R 664 .
+sudo find . -type d -name ".git" -prune -o -type d -exec chmod 775 {} \;
+sudo chown -R www-data:www-data .
+
+echo "**********************"
+echo "** Good work, tiger **"
+echo "**********************"
